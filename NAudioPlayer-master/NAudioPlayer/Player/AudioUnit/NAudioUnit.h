37f193a0904b9da102a79832b9278488f98f6a92
huchaojun <huchaojun@izunyou.com>
2020-05-27 16:44:52 +0800

    AudioUnit

---
+++ NAudioPlayer-master/NAudioPlayer/Player/AudioUnit/NAudioUnit.h
@@ -0,0 +1,17 @@
+//
+//  NAudioUnit.h
+//  NAudioUnit
+//
+//  Created by 泽娄 on 2019/9/18.
+//  Copyright © 2019 泽娄. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NAudioUnit : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
